bc89b3783973ac33ac8c91f1eedd80a54024406c
unknown <jjh5025d@gmail.com>
2026-03-29 01:55:01 +0900

    feat: 자동차 이름과 시도횟수 입력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,26 +1,32 @@
 package racingcar;
 import java.util.*;
-
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner scanner = new Scanner(System.in);
-
-        int round;
-        class Car {
-            String name;
-            int score = 0;
-
-            Car(String name) {
-                this.name = name;
-            }
+        ArrayList<Car> Cars = inputCars();
+        int round = inputRound();
+        for(int i = 0; i < Cars.size(); i++){
+            System.out.println(Cars.get(i).name);
         }
+        System.out.println(round);
+    }
 
+    static class Car {
+        String name;
+        int score = 0;
+
+        Car(String name) {
+            this.name = name;
+        }
+    }
+
+    private static ArrayList<Car> inputCars(){
         ArrayList<Car> Cars = new ArrayList<>();
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = scanner.nextLine();
+        String input = Console.readLine();
         String[] names = input.split(",");
 
         for(int i = 0; i < names.length; i++){
@@ -30,28 +36,25 @@ public class Application {
                 Cars.add(new Car(names[i]));
             }
         }
+        return Cars;
+    }
 
-        for(int i = 0; i < Cars.size(); i++){
-            for(int j = i + 1; j < Cars.size(); j++){
-
-            }
-        }
-
+    private static int inputRound(){
         System.out.println("시도할 회수는 몇회인가요?");
-
-        if (scanner.hasNextInt()) {
-            round = scanner.nextInt();
-        } else {
+        int round = 0;
+        try {
+            round = Integer.parseInt(Console.readLine().trim());
+            if(round <= 0){
+                throw new IllegalArgumentException();
+            }
+        } catch(NumberFormatException e){
             throw new IllegalArgumentException();
         }
 
         if(round <= 0){
             throw new IllegalArgumentException();
         }
-
-        for(int i = 0; i < Cars.size(); i++){
-            System.out.println(Cars.get(i).name);
-        }
-        System.out.println(round);
+        System.out.println(" ");
+        return round;
     }
 }
